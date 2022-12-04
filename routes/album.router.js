f3374d8c9c12e69a6f3c8aeeeb2297a4264c652e
Priscila Vieira <freedom.pva@gmail.com>
2022-12-03 22:03:55 -0300

    iteração 2.5

--- routes/album.router.js
+++ routes/album.router.js
@@ -1,21 +1,81 @@
 import express from "express"
 import AlbumModel from "../models/album.model.js"
-import PurchaseModel from "../models/purchase.model.js"
+
 
 
 const albumRoute = express.Router()
 
 // Iteração 2.1 - POST
 
-albumRoute.post("/creat-album", async (req, res) =>{
+albumRoute.post("/create-album", async (req, res) =>{
     try {
 
         const newAlbum = await AlbumModel.create(req.body)
-        return res.status(201).json(newAlbum)        
+        return res.status(200).json(newAlbum)        
     } catch (error) {
         console.log(error)
-        return response.status(500).json({msg: 'algo deu errado!'})
+        return response.status(400).json({msg: 'algo deu errado!'})
         
     }
 })
 
+// 2.2 - GET
+
+albumRoute.get("/all", async (req, res) => {
+    try {
+        const allAlbums = await AlbumModel.find()
+        return res.status(200).json(allAlbums)        
+    } catch (error) {
+        console.log(error)
+        return response.status(400).json({msg: 'algo deu errado!'})
+        
+    }
+})
+
+// 2.3 GET BY ID
+
+albumRoute.get("/:albumId", async ( req, res) => {
+    try {
+        const {albumId} = req.params
+        const albumById = await AlbumModel.findById(albumId)
+        return res.status(200).json(albumById)        
+    } catch (error) {
+        console.log(error)
+        return response.status(400).json({msg: 'algo deu errado!'})
+        
+    }
+})
+
+// 2.4 - PUT
+
+albumRoute.put("update/:albumId", async (req, res) =>{
+    try {
+
+        const {albumId} = req.params
+        const updatedAlbum = await AlbumModel.findByIdAndUpdate(
+            { _id: albumId },
+            {...req.body},
+            {new:true, runValidators: true}
+        )
+        return res.status(200).json(updatedAlbum)        
+    } catch (error) {
+        console.log(error)
+        return response.status(400).json({msg: 'algo deu errado!'})
+        
+    }
+})
+
+
+  // 2.5 - DELETE
+
+  albumRoute.delete("/delete/:albumId", async(req, res) => {
+    try {
+        const {albumId} = req.params
+        const deletedAlbum = await AlbumModel.findByIdAndDelete(albumId)
+        return res.status(200).json(deletedAlbum)        
+    } catch (error) {
+        console.log(error)
+        return response.status(400).json({msg: 'algo deu errado!'})
+        
+    }
+  })
